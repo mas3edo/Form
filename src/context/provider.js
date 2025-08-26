c37d75f87c1ae5b93f61f362949f361b06454599
Hp <ahmedmasshoud929@gmail.com>
2025-08-26 03:58:30 +0300

    Update project files before deployment

--- src/context/provider.js
+++ src/context/provider.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import { MyContext } from "./context";
 function MyProvider({ children }) {
@@ -30,40 +29,4 @@ function MyProvider({ children }) {
     </MyContext.Provider>
   );
 }
-
 export default MyProvider;
-=======
-import { useState } from "react";
-import { MyContext } from "./context";
-function MyProvider({ children }) {
-  const [user, setUser] = useState({
-    FristeName: "",
-    SecondName: "",
-    LastName: "",
-    Gender: "",
-    id: "",
-    Adress: "",
-    BirthDate: "",
-    phone: "",
-    email: "",
-    Housing: "",
-    Education: "",
-    School: "",
-    AptitudeTest: 0,
-    AchievementTest: "",
-    HighSchoolCertificate: "",
-    AptitudeExamFacility: "",
-    FirstDesire: "",
-    SecondDesire: "",
-    ThirdDesire: "",
-  });
-
-  return (
-    <MyContext.Provider value={{ user, setUser }}>
-      {children}
-    </MyContext.Provider>
-  );
-}
-
-export default MyProvider;
->>>>>>> 2c610e7f7143eb05735470cec7fb25ce9099df84
